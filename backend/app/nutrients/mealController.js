61814654365a0df1d928cf73d97292c8aeef90df
Helwa <helwasalameh14@gmail.com>
2025-05-29 18:55:08 -0500

    Updated the mealController.js

--- backend/app/nutrients/mealController.js
+++ backend/app/nutrients/mealController.js
@@ -3,14 +3,21 @@ const Meal = require('../models/Meal');
 exports.createMeal = async (req, res) => {
   try {
     const { userId, description, calories, protein, carbs, fats } = req.body;
-    
+
+    if (!userId || !description) {
+      return res.status(400).json({
+        success: false,
+        error: 'userId and description are required'
+      });
+    }
+
     const meal = await Meal.create({
       userId,
       description,
-      calories,
-      protein,
-      carbs,
-      fats
+      calories: Number(calories) || 0,
+      protein: Number(protein) || 0,
+      carbs: Number(carbs) || 0,
+      fats: Number(fats) || 0
     });
 
     res.status(201).json({
