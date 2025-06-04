ad16c72326ef625504f8d5cb65af80df4a8d74dd
rameldawood2025 <rdawood1@depaul.edu>
2025-06-04 17:27:48 -0500

    added log out button

--- fitness-tracker-app/App.js
+++ fitness-tracker-app/App.js
@@ -1,18 +1,38 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { NavigationContainer, DarkTheme } from '@react-navigation/native';
 import { createDrawerNavigator } from '@react-navigation/drawer';
-import { View, StatusBar, TouchableOpacity } from 'react-native';
+import { View, StatusBar, ActivityIndicator, Text } from 'react-native';
 import { MaterialCommunityIcons } from '@expo/vector-icons';
+
 
 import HomeScreen from './src/screens/HomeScreen';
 import WorkoutsScreen from './src/screens/WorkoutsScreen';
 import MealsScreen from './src/screens/MealsScreen';
 import ProfileScreen from './src/screens/ProfileScreen';
-import AuthScreen from './src/screens/AuthScreen.js';
-
+import AuthScreen from './src/screens/AuthScreen';
 
 const Drawer = createDrawerNavigator();
+
+function LogoutScreen({ navigation }) {
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      navigation.reset({
+        index: 0,
+        routes: [{ name: 'Log-In' }],
+      });
+    }, 1000); 
+
+    return () => clearTimeout(timer);
+  }, [navigation]);
+
+  return (
+    <View style={{ flex: 1, backgroundColor: '#121212', justifyContent: 'center', alignItems: 'center' }}>
+      <ActivityIndicator size="large" color="#fff" />
+      <Text style={{ color: '#fff', marginTop: 10 }}>Logging out...</Text>
+    </View>
+  );
+}
 
 export default function App() {
   return (
@@ -33,9 +53,9 @@ export default function App() {
             drawerLabelStyle: {
               color: '#fff',
             },
-                        headerRight: () => ( 
+            headerRight: () => (
               <MaterialCommunityIcons
-                name="weight-lifter" 
+                name="weight-lifter"
                 size={26}
                 color="#fff"
                 style={{ marginRight: 15 }}
@@ -48,7 +68,7 @@ export default function App() {
           <Drawer.Screen name="Profile" component={ProfileScreen} />
           <Drawer.Screen name="Workouts" component={WorkoutsScreen} />
           <Drawer.Screen name="Meals" component={MealsScreen} />
-          
+          <Drawer.Screen name="Log Out" component={LogoutScreen} />
         </Drawer.Navigator>
       </NavigationContainer>
     </GestureHandlerRootView>
